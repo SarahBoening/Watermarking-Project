95841e8508c73fc16178addfc6e2dc0ea8ea1b88
g.luensdorf <g.luensdorf@gmail.com>
2019-07-27 12:15:05 +0200

    adds detector structure

--- Detector.py
+++ Detector.py
@@ -5,6 +5,8 @@ to compute a similarity value. Compares similiraty value with a threshold value 
 to decide if the extracted and given watermark are the same.
 """
 
+import math as m
+
 def detect(given_wm, stegowork, coverwork, p, q):
     """
     Execute detection pipeline.
@@ -16,17 +18,54 @@ def detect(given_wm, stegowork, coverwork, p, q):
 
     returns True or False - given_wm is present in the stegowork.
     """
+    TAU = 0.7
 
-    features = extract_features(coverwork)
+    path = BBS(p, q) # used in the extractor functions?
+    stego_features = extract_features(stegowork) # probably replace with modified_DCT()
+    cover_features = extract_features(coverwork) # probably replace with modified_DCT()
+    extracted_wm = extract_watermark(stego_features, cover_features)
+    similarity = calc_similarity(given_wm, extracted_wm)
+    if similarity > TAU:
+        return True
+    else:
+        return False
 
-def extract_features(coverwork):
+def extract_features(image):
     """
-    Use a modified DCT to extract features from coverwork.
+    Use a modified DCT to extract features from an image.
 
-    coverwork: multi-dimensional list, original coverwork without watermark
+    image: multi-dimensional list, an image
 
     returns list of tuples, list contains feature positions
     """
     # features = modified_DCT(coverwork)
     # return features
     pass
+
+def extract_watermark(stego_features, cover_features):
+    """
+    Extract watermark using the inversed embedding function.
+
+    stego_features: list of tuples, list contains feature positions
+    cover_features: list of tuples, list contains feature positions
+
+    returns list, contains the extracted watermark
+    """
+    watermark = inverse_modified_DCT(stego_features, cover_features)
+    pass
+
+def calc_similarity(given_wm, extracted_wm):
+    """
+    Calculates a similarity value for given_wm and extracted_wm.
+
+    given_wm: list, contains the given watermark
+    extracted_wm: list, contains the extracted watermark
+
+    returns: float, calculated similarity value
+    """
+
+    numerator = extracted_wm * given_wm
+    denominator = m.sqrt(extracted_wm * extracted_wm) # basic spread spectrum scheme
+    # denominator = m.sqrt((extracted_wm * extracted_wm) * (given_wm * given_wm))
+    similarity = numerator / denominator
+    return similarity
