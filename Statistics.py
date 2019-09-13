5c3be0c786574b106dca32417d429c3fbbb8d53a
Sarah Böning <Sarah.boening@tu-clausthal.de>
2019-09-13 11:16:19 +0200

    added a method for chi2 test

--- Statistics.py
+++ Statistics.py
@@ -5,5 +5,36 @@ File to add all methods that we need for the analysis
 '''
 
 
-def chisquaretest(values):
-    pass
+def chisquaretestcorr(values):
+    '''
+    test for correlated pairs
+    :param values: histogram counts
+    :return: chi^2 value y
+    '''
+    y = 0
+    num = int(np.floor(values.shape[0]/2))
+    for i in range(1, num):
+        ex = (values[2*i] + values[2*i+1]) / 2
+        di = pow((values[2*i] - ex), 2)
+        if ex != 0:
+            y += di/ex
+    print(i)
+    return y
+
+
+def chisquaretestuncorr(values):
+    '''
+    test for uncorrelated pairs
+    '''
+    y = 0
+    num = int(np.floor(values.shape[0]/2))
+    for i in range(1, num):
+        ex = (values[2*i] + values[2*i-1]) / 2
+        di = pow((values[2*i] - ex), 2)
+        if ex != 0:
+            y += di/ex
+    return y
+
+
+def subhistcount(values, min=0, max=255):
+    return np.histogram(values, bins=list(range(min, max+2)))[0]
